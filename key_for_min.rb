15d172c98366370bfcc035194b4fa2e5d7a6de9f
Yunhao Gao <as510891328@gmail.com>
2020-08-12 20:12:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 999
-  name = ""
   if name_hash
+    min = 999
+    name = ""
     name_hash.each { |key, value|
       if value < min
         min = value
